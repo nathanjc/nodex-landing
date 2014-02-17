ea04a10d6b38596c6e70b9063619b393a4ffc0ea
Nathan Chapman <hisbrothernathan@gmail.com>
2014-02-17 12:20:16 -0800

    Updates and fixes

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/subscribed', routes.subscribed);
+app.get('/manifesto', routes.manifesto);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Nodex' });
 };
+
+exports.subscribed = function(req, res) {
+  res.render('subscribed', { title: 'Nodex' });
+};
+
+exports.manifesto = function(req, res) {
+  res.render('manifesto', { title: 'Nodex' });
+};
